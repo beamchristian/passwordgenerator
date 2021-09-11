096c4f09c477a7f5207b5e48d96d768d454b2ef2
beamchristian <beamchristian@yahoo.com>
2021-09-10 20:35:30 -0400

    Restructed to use alerts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,79 @@
-// Assignment code here
-const empty = "";
-const capitol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "~!@#$%^&*-_";
+window.addEventListener('load', function () {
+    var passLength = prompt("How many characters would you like your password to be?");
 
-const passwordLength = document.getElementById("p-length");
-const upperCase = document.getElementById("p-uppercase");
-const lowerCase = document.getElementById("p-lowercase");
-const pNumber = document.getElementById("p-number");
-const pSymbol = document.getElementById("p-symbol");
-const submit = document.getElementById("submit");
-const password = document.getElementById("password");
+    while (passLength < 8 || passLength > 128) {
+        passLength = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+    }
 
-submit.addEventListener("click", () => {
-  let initialPassword = empty;
-    (upperCase.checked) ? initialPassword += capitol : "";
-    (lowerCase.checked) ? initialPassword += lowCase : "";
-    (pNumber.checked) ? initialPassword += number : "";
-    (pSymbol.checked) ? initialPassword += symbol : "";
+    var passUppercase = confirm("Would you like uppercase letters included?");
+    var passLowercase = confirm("Would you like lowercase letters included?");
+    var passNumbers = confirm("Would you like numbers included?");
+    var passSymbols = confirm("would you like special characters included?");
 
-    password.value = generatePassword(passwordLength.value, initialPassword)
+    while (!(passUppercase || passLowercase || passNumbers || passSymbols)) {
+        alert("You must select at least one character type!");
+
+        passUppercase = confirm("Would you like uppercase letters included?");
+        passLowercase = confirm("Would you like lowercase letters included?");
+        passNumbers = confirm("Would you like numbers included?");
+        passSymbols = confirm("Would you like special characters included?");
+    }
+
+    const resultEl = document.getElementById('password');
+
+    document.getElementById('generate').addEventListener('click', () => {
+        resultEl.value = generatePassword(passLowercase, passUppercase, passNumbers, passSymbols, passLength);
+    });
+
 });
 
-function generatePassword(l, initialPassword) {
-    let pass = "";
-    for (let i = 0; i < l; i++) {
-      pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
 
+const randomFunction = {
+    lower: randomLower,
+    upper: randomUpper,
+    number: randomNumber,
+    symbol: randomSymbol
+};
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArray = [{
+        lower
+    }, {
+        upper
+    }, {
+        number
+    }, {
+        symbol
+    }].filter(item => Object.values(item)[0]);
+
+    // create a loop
+    for (let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunction[funcName]();
+        });
     }
-    return pass;
+
+    const finalPass = generatedPassword.slice(0, length);
+
+    return finalPass;
 }
 
+// Generator functions
+function randomLower() {
+    return rando("qwertyuiopasdfghjklzxcvbnm");
+}
+
+function randomUpper() {
+    return rando("QWERTYUIOPASDFGHJKLZXCVBNM");
+}
+
+function randomNumber() {
+    return rando(9);
+}
+
+function randomSymbol() {
+    return rando('!@#$%^&*(){}[]=<>/,.');
+}
